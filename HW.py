3a8514a45071d33508029601b7b3ac17db020094
Divya-14-11 <divyapasadpatro@gmail.com>
2026-03-03 21:01:24 +0530

    save

---
+++ HW.py
@@ -0,0 +1,8 @@
+# Take binary input from the user
+binary = input("Enter a binary number: ")
+
+# Convert binary to decimal
+decimal = int(binary, 2)
+
+# Print the result
+print("The decimal value is:", decimal)
